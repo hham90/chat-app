0186f9b6b68d943001f6f44ae2bc3e9319ef6d1d
hham90 <148152147+hham90@users.noreply.github.com>
2024-02-29 18:28:16 -0500

    added firebase
    added firebase config to app.js

--- App.js
+++ App.js
@@ -4,10 +4,24 @@ import Start from './components/Start'
 import Chat from './components/Chat'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"])
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyD61v9hnX8WT279Zb0wcJBcnrw3LqXdmKY",
+    authDomain: "chatapp-e9406.firebaseapp.com",
+    projectId: "chatapp-e9406",
+    storageBucket: "chatapp-e9406.appspot.com",
+    messagingSenderId: "196986032724",
+    appId: "1:196986032724:web:dfe74bd33ec7826528d3c1"
+  };
+  const app = initializeApp(firebaseConfig)
+  const db = getFirestore(app);
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -18,9 +32,9 @@ const App = () => {
           component={Start}
         />
         <Stack.Screen
-          name="Chat"
-          component={Chat}
-        />
+          name="Chat">
+          {props => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
